8c893a6409b654d03a10b7caa43398845f221bad
raphael <raphael.birkl@gmail.com>
2023-12-14 14:05:06 +0100

    main tasks solved

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,41 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white70),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      backgroundColor: Colors.white30,
+      body: Container(
+          child: const Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text("23°",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 58,
+                    fontWeight: FontWeight.bold)),
+            Spacer(),
+            Text("Munich", style: TextStyle(color: Colors.black, fontSize: 24)),
+            Spacer(),
+            Text("-beergarden suitable-",
+                style: TextStyle(color: Colors.grey, fontSize: 24)),
+            Spacer(),
+          ],
+        ),
+      )),
+    );
   }
 }
